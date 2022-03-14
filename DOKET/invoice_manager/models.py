fab7f483168d76fed46a32848f3ff9b4f6fecbcf
Bodybro <ivan.stereotekk@gmail.com>
2022-03-15 01:02:47 +0400

    some model

--- DOKET/invoice_manager/models.py
+++ DOKET/invoice_manager/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Customer(AbstractUser):
+    KINDS = (
+        ('R','Ресторан'),
+        ('C','Кафе'),
+        ('H','Отель'),
+        ('M','Магазин'),
+        ('R','Торговая Фирма'),
+        ('X','Предприниматель'),
+        (None,'Неустановленный тип предприятия'),
+    )
+    title = models.CharField(max_length=70,verbose_name='Наименование')
+    adress = models.CharField(max_length=100, verbose_name='Адрес')
+    id_int = models.CharField(max_length=70, verbose_name='ID организации')
+    kind = models.CharField(max_length=1,choices=KINDS,)
+
+class Item(models.Model):
+
